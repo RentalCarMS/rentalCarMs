045e0f9a1e78a3c9128aa794837541a945ff6583
Msaglam66 <94043328+Msaglam66@users.noreply.github.com>
2021-12-08 22:02:17 +0300

    08-12-2021(swagger is working)

--- RentalAppApiRentals/src/main/java/com/appsdeveloperblog/rentalapp/api/rentals/RentalAppApiRentalsApplication.java
+++ RentalAppApiRentals/src/main/java/com/appsdeveloperblog/rentalapp/api/rentals/RentalAppApiRentalsApplication.java
@@ -4,12 +4,15 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import com.appsdeveloperblog.rentalapp.api.rentals.core.utilities.mapping.ModelMapperService;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableSwagger2
+@EnableSwagger2
 public class RentalAppApiRentalsApplication {
 
 
@@ -18,24 +21,22 @@ public class RentalAppApiRentalsApplication {
 		SpringApplication.run(RentalAppApiRentalsApplication.class, args);
 
 	}
-
-	/*@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.appsdeveloperblog.rentalapp.api.rentals.RentalAppApiRentals"))
-				.build();
-	}*/
-/*	@Bean
-	public ModelMapper getModelMapper() {
-		ModelMapper modelMapper=new ModelMapper();
-		return modelMapper;
-	}*/
-
 	@Bean
 	public ModelMapper getModelMapper(){
 
 		ModelMapper modelMapper = new ModelMapper();
 		return modelMapper;
 	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors
+				.basePackage("com.appsdeveloperblog.rentalapp.api.rentals"))
+				.build();
+	}
+
+
+
 }
